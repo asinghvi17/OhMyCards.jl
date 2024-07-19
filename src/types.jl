c7481fc10f587423721322284d0e5e4c3b83c3e9
Anshul Singhvi <anshulsinghvi@gmail.com>
2024-07-19 15:55:35 +0530

    upgrade ExampleConfig (first of many)

--- src/types.jl
+++ src/types.jl
@@ -15,7 +15,16 @@ end
 
 
 @kwdef struct ExampleConfig <: Documenter.Plugin
+    """
+    The features to be included in the example gallery.
+    """
     features::Vector{Feature} = Feature[CopyPastableExample()]
+    """
+    The known examples to be included in the example gallery.
+    """
     known_examples::Vector = []
+    "Whether to inject scoped CSS into a div enclosing the output of each gallerybox "
+    inject_scoped_css = false
+    "For internal use only, acts as a global cache to store the results from `@cardmeta` for each page"
     gallery_dict::Dict = Dict{String, Any}()
 end
