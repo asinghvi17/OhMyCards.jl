cd7637ce6bc8957c64d4705d5d57fed7890a5418
Anshul Singhvi <anshulsinghvi@gmail.com>
2024-07-20 10:53:04 +0530

    Add some debug info

--- docs/make.jl
+++ docs/make.jl
@@ -1,7 +1,17 @@
 using OhMyCards
 using Documenter
+using Literate
+using quarto_jll
 
 DocMeta.setdocmeta!(OhMyCards, :DocTestSetup, :(using OhMyCards); recursive=true)
+
+# Markdownify the Literate logo example
+
+# Literate.markdown(joinpath(@__DIR__, "logo.jl"), joinpath(@__DIR__, "src"); documenter = true)
+
+# cp(joinpath(dirname(pathof(OhMyCards)), "gallery_style.css"), joinpath(@__DIR__, "src", "gallery_style.css"))
+
+plugins = [ExampleConfig(; inject_scoped_css = true)]
 
 makedocs(;
     modules=[OhMyCards],
@@ -15,7 +25,11 @@ makedocs(;
     pages=[
         "Home" => "index.md",
     ],
+    plugins,
+    warnonly = true,
 )
+
+@show plugins[1].gallery_dict
 
 deploydocs(;
     repo="github.com/asinghvi17/OhMyCards.jl",
