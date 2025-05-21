abb444b86970cfdb41c3adc33a5757bc4f9825f2
Anshul Singhvi <anshulsinghvi@gmail.com>
2025-05-21 15:21:13 -0400

    make makie ext cover more robust

--- ext/OhMyCardsMakieExt.jl
+++ ext/OhMyCardsMakieExt.jl
@@ -9,23 +9,23 @@ import OhMyCards: get_image_url, set_cover_to_image!
 
 
 function OhMyCards.get_image_url(page, doc, fig::Makie.FigureLike)
-    try
-        image = Makie.colorbuffer(fig)
+    img = try
+        Makie.colorbuffer(fig)
     catch e
         @error "Error while saving Makie figure in the card!" page=page.source 
-        rethrow(e)
-    else
-        iob = IOBuffer()
-        ImageIO.save(FileIO.Stream{FileIO.format"PNG"}(iob), img)
-        # We could include this inline, but that seems to be causing issues.
-        # meta[:Cover] = "data:image/png;base64, " * Base64.base64encode(String(take!(iob)))
-        # Instead, we will save to a file and include that.
-        bytes = take!(iob)
-        filename = string(hash(bytes), base = 62) * ".png"
-        path = joinpath(page.workdir, filename)
-        write(path, bytes)
-        return "/" * joinpath(relpath(page.workdir, doc.user.build), filename)
+        @error e
+        reshape([Makie.RGBf(1, 0, 0)], 1, 1)
     end
+    iob = IOBuffer()
+    ImageIO.save(FileIO.Stream{FileIO.format"PNG"}(iob), img)
+    # We could include this inline, but that seems to be causing issues.
+    # meta[:Cover] = "data:image/png;base64, " * Base64.base64encode(String(take!(iob)))
+    # Instead, we will save to a file and include that.
+    bytes = take!(iob)
+    filename = string(hash(bytes), base = 62) * ".png"
+    path = joinpath(page.workdir, filename)
+    write(path, bytes)
+    return "/" * joinpath(relpath(page.workdir, doc.user.build), filename)
 end
 
 function OhMyCards.set_cover_to_image!(meta, page, doc, fig::Makie.FigureLike)
