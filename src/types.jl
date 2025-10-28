eb3898cd7323b311f68347711c04f92cf5851b1f
Anshul Singhvi <anshulsinghvi@gmail.com>
2025-10-27 20:39:32 -0400

    Add dot_slash option to types.jl

--- src/types.jl
+++ src/types.jl
@@ -25,6 +25,8 @@ end
     known_examples::Vector = []
     "Whether to inject scoped CSS into a div enclosing the output of each gallerybox "
     inject_scoped_css = false
+    "Whether to use `./examples` or just `examples` for paths"
+    dot_slash = false
     "For internal use only, acts as a global cache to store the results from `@cardmeta` for each page"
     gallery_dict::Dict = Dict{String, Any}()
 end
